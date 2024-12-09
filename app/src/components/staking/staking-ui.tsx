4ed283ebd715dc117accef11c380af4e0bbc1e59
titrabui <titrabui@gmail.com>
2024-12-09 15:44:14 +0700

    display token balance

--- app/src/components/staking/staking-ui.tsx
+++ app/src/components/staking/staking-ui.tsx
@@ -2,6 +2,7 @@ import { PublicKey } from '@solana/web3.js'
 import { useMemo, useState } from 'react'
 import { useStakingProgram, useStakingProgramAccount } from './staking-data-access'
 import { BN } from '@coral-xyz/anchor'
+import { useGetTokenAccounts } from '../account/account-data-access'
 
 function Stake(props: Readonly<StakingProps>) {
   const { depositMutation } = useStakingProgramAccount({
@@ -67,14 +68,15 @@ function StakingPoolInfo() {
         <div>APY</div>
       </div>
 
-
       <div
         className="flex justify-between items-center mb-6"
         style={{
           borderBottom: '1px solid',
         }}
       >
-        <div className="text-2xl font-bold text-green-500">{data ? new Date(data.startTime.toNumber() / 1000).toLocaleDateString() : ''}</div>
+        <div className="text-2xl font-bold text-green-500">
+          {data ? new Date(data.startTime.toNumber() / 1000).toLocaleDateString() : ''}
+        </div>
         <div>START TIME</div>
       </div>
 
@@ -84,33 +86,52 @@ function StakingPoolInfo() {
           borderBottom: '1px solid',
         }}
       >
-        <div className="text-2xl font-bold text-green-500">{data ? new Date(data.endTime.toNumber() / 1000).toLocaleDateString() : ''}</div>
+        <div className="text-2xl font-bold text-green-500">
+          {data ? new Date(data.endTime.toNumber() / 1000).toLocaleDateString() : ''}
+        </div>
         <div>END TIME</div>
       </div>
     </div>
   )
 }
 
-function UserStakeInfo(props: Readonly<StakingProps>) {
+function UserTokenBalance({ mint, address }: { mint: PublicKey | undefined; address: PublicKey }) {
+  const query = useGetTokenAccounts({ address })
+  const stakeToken = useMemo(
+    () => query.data?.find((item) => mint && item.account.data.parsed.info.mint.toString() === mint.toString()),
+    [query.data, mint],
+  )
+
+  return (
+    <div className="flex justify-between items-center mb-6">
+      <div>Your Token Balance</div>
+      <span className="font-bold">{stakeToken?.account.data.parsed.info.tokenAmount.uiAmountString} NPG</span>
+    </div>
+  )
+}
+
+function UserStakedBalance(props: Readonly<StakingProps>) {
   const { accountQuery } = useStakingProgramAccount({
     account: props.address,
   })
   const userInfo = useMemo(() => accountQuery.data ?? null, [accountQuery.data])
 
   return (
+    <div className="flex justify-between items-center mb-6">
+      <div>Your Token Staked</div>
+      <span className="font-bold">{new BN(userInfo?.stakedAmount).toString()} NPG</span>
+    </div>
+  )
+}
+
+function UserStakeInfo(props: Readonly<StakingProps>) {
+  const { getStakingInfo } = useStakingProgram()
+  const data = getStakingInfo.data
+
+  return (
     <div>
-      <div className="flex justify-between items-center mb-6">
-        <div>Your Token Balance</div>
-        <p>
-          <span className="font-bold">{new BN(userInfo?.stakedAmount).toString()} NPG</span>
-        </p>
-      </div>
-      <div className="flex justify-between items-center mb-6">
-        <div>Your Token Staked</div>
-        <p>
-          <span className="font-bold">{new BN(userInfo?.stakedAmount).toString()} NPG</span>
-        </p>
-      </div>
+      <UserTokenBalance mint={data?.tokenMintAddress} address={props.address} />
+      <UserStakedBalance address={props.address} />
     </div>
   )
 }
